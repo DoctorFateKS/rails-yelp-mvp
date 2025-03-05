2dcdd4e053236b6e256c361ca7bc0ba798fabe5c
Kevin Saison <kevin.saison91@gmail.com>
2025-03-05 12:19:32 +0100

    Add 5 instances with seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Cleaning the database
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+# Create the instances
+puts 'Creating restaurants...'
+5.times do
+  Restaurant.create!(name: Faker::Kpop.girl_groups,
+  address: Faker::Fantasy::Tolkien.location,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+  phone_number: Faker::PhoneNumber.phone_number)
+end
+puts "Finished! Created #{Restaurant.count} restaurants."
